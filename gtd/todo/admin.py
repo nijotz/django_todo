e353e844a49379452f123284a07431af89ae21fd
Nick Tzaperas <nick@nijotz.com>
2013-09-26 19:04:21 -0700

    todo_lists now uses a filter_horizontal widget in the admin

--- gtd/todo/admin.py
+++ gtd/todo/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 from gtd.todo.models import TodoList
 from gtd.todo.models import TodoItem
 
-admin.site.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    model = TodoItem
+    filter_horizontal = ('todo_lists',)
+
+admin.site.register(TodoItem, TodoItemAdmin)
 admin.site.register(TodoList)
